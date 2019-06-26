3b53ab35bf80e3037da3f1adc7f74e4842268873
sztojkatamas <sztojka.tamas@gmail.com>
2019-06-26 17:10:40 +0200

    Create OutsideOfSealedClass.kt

---
+++ src/main/kotlin/com/kotlin/demo/ch3/OutsideOfSealedClass.kt
@@ -0,0 +1,15 @@
+package com.kotlin.demo.ch3
+
+/*
+// This does not work in this file!
+class Other2:SealedClass() {
+
+}
+*/
+
+//class Persona:OtherClass()
+
+fun main() {
+    val other = OtherClass()
+    other.secretFunction()
+}
